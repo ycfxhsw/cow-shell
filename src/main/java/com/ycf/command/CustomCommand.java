bd6fdd43953ac3649ebbcff0d90fd476bce7009e
ycfxhsw <ycfmail@qq.com>
2021-11-07 19:19:20 +0800

    读取自定义图像

--- src/main/java/com/ycf/command/CustomCommand.java
+++ src/main/java/com/ycf/command/CustomCommand.java
@@ -1,12 +1,14 @@
 package com.ycf.command;
 
 import com.ycf.symbol.Symbol;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.shell.standard.ShellCommandGroup;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 import org.springframework.shell.standard.ShellOption;
 
 import java.io.File;
+import java.io.IOException;
 
 /**
  * @author ycf
@@ -14,21 +16,32 @@ import java.io.File;
 @ShellComponent
 @ShellCommandGroup("Custom")
 public class CustomCommand {
-    private static final String COW_PATH = "src\\main\\resources\\symbol\\cow.txt";
-    private static final String BUDDHA = "src\\main\\resources\\symbol\\buddha.txt";
-
     @ShellMethod(key = "cowsay", value = "小母牛说话")
     public String cowSay(@ShellOption(defaultValue = "hello cow!", help = "小母牛要说的话") String say,
                          @ShellOption(value = {"-e", "-eye"}, defaultValue = "oo", help = "小母牛眼睛形状,默认为oo") String eyes) {
-        return Symbol.cowSayService(say, eyes, COW_PATH);
+        ClassPathResource cpr = new ClassPathResource("symbol/cow.txt");
+        String path = null;
+        try {
+            path = cpr.getURL().getPath();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return Symbol.cowSayService(say, eyes, path);
     }
 
-    @ShellMethod(key = {"-b", "-buddha"}, value = "佛祖像")
+    @ShellMethod(key = {"cow -b", "cow -buddha"}, value = "佛祖像")
     public String buddha() {
-        return Symbol.getContent(BUDDHA);
+        ClassPathResource cpr = new ClassPathResource("symbol/buddha.txt");
+        String path = null;
+        try {
+            path = cpr.getURL().getPath();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return Symbol.getContent(path);
     }
 
-    @ShellMethod(key = {"-c", "-custom"}, value = "自定义图像【e.g: -c custom.txt】")
+    @ShellMethod(key = {"cow -c", "cow -custom"}, value = "自定义图像【e.g: -c custom.txt】")
     public String custom(@ShellOption(value = {"-p", "-path"}, help = "自定义图像文本路径名【e.g: c:/custom.txt】") String path) {
         File file = new File(path);
         String absolutePath = file.getAbsolutePath();
